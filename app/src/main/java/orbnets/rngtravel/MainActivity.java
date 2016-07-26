1535c2595596de5a7ef4e5c34d3e335bf486fbd4
moomoo101 <danielstenbro101@hotmail.com>
2016-07-26 00:54:49 -0500

    Spinner update
    *Fixes to files layout. Removed Split between spinner and main activity
    * updated layout for spinner dropdown

--- app/src/main/java/orbnets/rngtravel/MainActivity.java
+++ app/src/main/java/orbnets/rngtravel/MainActivity.java
@@ -1,10 +1,14 @@
 package orbnets.rngtravel;
 
 
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 // google maps API libraries
 import com.google.android.gms.maps.GoogleMap;
@@ -14,9 +18,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback,AdapterView.OnItemSelectedListener {
 
-    private GoogleMap mGmap;
+    protected GoogleMap mGmap;
+    protected int mapType;
+    Spinner drop_down;
+
+    public MainActivity(int mapType) {
+        this.mapType = mapType;
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +36,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             if (mGmap == null) {
                 MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
                 mapFragment.getMapAsync(this);
+                onMapReady(mGmap);
+
             }
         }
         catch (Exception e) {
@@ -35,20 +47,71 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void onMapReady(GoogleMap googleMap) {
+
+        googleMap.setMapType(mapType);
         googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
         googleMap.setMyLocationEnabled(true);
-        generateSpinner();
 
+        generateSpinner(googleMap);
+        drop_down.setOnItemSelectedListener(this);
     }
+
 
     // create the drop down menu for users to choose the type of map they want to switch between
 
-    private void generateSpinner() {
+    private void generateSpinner(GoogleMap googleMap) {
 
-        Spinner drop_down = (Spinner) findViewById(R.id.maptype_spinner);
+        drop_down = (Spinner) findViewById(R.id.maptype_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.maptype_array,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         drop_down.setAdapter(adapter);
-
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+ /*
+         check the text which was selected from the drop down.
+         The spinner drop down should appear as the following...
+
+         Normal
+         Satellite
+         Terrain
+         Hybrid
+
+         then if nothing else, choose "none"
+
+*/
+        TextView item = (TextView) view;
+
+        if(parent.getItemAtPosition(position) == "Normal"){
+
+            this.mapType = GoogleMap.MAP_TYPE_NORMAL;
+
+        }
+        if(parent.getItemAtPosition(position) == "Satellite"){
+
+            this.mapType = GoogleMap.MAP_TYPE_SATELLITE;
+
+        }
+        if(parent.getItemAtPosition(position) == "Terrain"){
+
+            this.mapType = GoogleMap.MAP_TYPE_TERRAIN;
+
+        }
+        if(parent.getItemAtPosition(position) == "Hybrid"){
+
+            this.mapType = GoogleMap.MAP_TYPE_HYBRID;
+
+        }
+        if(parent.getItemAtPosition(position) == "None"){
+            this.mapType = GoogleMap.MAP_TYPE_NONE;
+        }
+    }
+
+
+        public void onNothingSelected(AdapterView<?> parent) {
+        // if nothing selected then do nothing
+    }
+
 }
