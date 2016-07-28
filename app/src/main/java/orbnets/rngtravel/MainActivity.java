484a6dbcfc07584ca9d2d0e443677c0d9c2a583e
moomoo101 <danielstenbro101@hotmail.com>
2016-07-27 20:21:47 -0500

    Added feature - Multiple Map Types
    added functionality to switch between map types using a spinner drop
    down.

--- app/src/main/java/orbnets/rngtravel/MainActivity.java
+++ app/src/main/java/orbnets/rngtravel/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 // google maps API libraries
 import com.google.android.gms.maps.GoogleMap;
@@ -21,28 +22,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback,AdapterView.OnItemSelectedListener {
 
     protected GoogleMap mGmap;
-    protected int mapType;
+    protected int mapType = 1;
     Spinner drop_down;
-
-    public MainActivity(int mapType) {
-        this.mapType = mapType;
-    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            if (mGmap == null) {
-                MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-                mapFragment.getMapAsync(this);
-                onMapReady(mGmap);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        generateSpinner();
+        drop_down.setOnItemSelectedListener(this);
 
     }
 
@@ -52,19 +43,33 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
         googleMap.setMyLocationEnabled(true);
 
-        generateSpinner(googleMap);
-        drop_down.setOnItemSelectedListener(this);
     }
 
 
     // create the drop down menu for users to choose the type of map they want to switch between
 
-    private void generateSpinner(GoogleMap googleMap) {
+    private void generateSpinner() {
 
         drop_down = (Spinner) findViewById(R.id.maptype_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.maptype_array,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         drop_down.setAdapter(adapter);
+    }
+
+    private void createMap(GoogleMap map, int mapType){
+
+        try {
+            if (map == null) {
+
+                MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+                mapFragment.getMapAsync(this);
+                onMapReady(map);
+
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -84,29 +89,41 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 */
         TextView item = (TextView) view;
 
-        if(parent.getItemAtPosition(position) == "Normal"){
-
-            this.mapType = GoogleMap.MAP_TYPE_NORMAL;
-
-        }
-        if(parent.getItemAtPosition(position) == "Satellite"){
-
-            this.mapType = GoogleMap.MAP_TYPE_SATELLITE;
+        if(item.getText().equals("Normal")){
+            Toast.makeText(this, "Changing map type to "+item.getText(),Toast.LENGTH_SHORT).show();
+            mapType = GoogleMap.MAP_TYPE_NORMAL;
+            createMap(mGmap, mapType);
 
         }
-        if(parent.getItemAtPosition(position) == "Terrain"){
 
-            this.mapType = GoogleMap.MAP_TYPE_TERRAIN;
-
-        }
-        if(parent.getItemAtPosition(position) == "Hybrid"){
-
-            this.mapType = GoogleMap.MAP_TYPE_HYBRID;
+        if(item.getText().equals("Satellite")){
+            Toast.makeText(this, "Changing map type to "+item.getText(),Toast.LENGTH_SHORT).show();
+            mapType = GoogleMap.MAP_TYPE_SATELLITE;
+            createMap(mGmap, mapType);
 
         }
-        if(parent.getItemAtPosition(position) == "None"){
-            this.mapType = GoogleMap.MAP_TYPE_NONE;
+
+        if(item.getText().equals("Terrain")){
+            Toast.makeText(this, "Changing map type to "+item.getText(),Toast.LENGTH_SHORT).show();
+            mapType = GoogleMap.MAP_TYPE_TERRAIN;
+            createMap(mGmap, mapType);
+
+
         }
+        if(item.getText().equals("Hybrid")){
+            Toast.makeText(this, "Changing map type to "+item.getText(),Toast.LENGTH_SHORT).show();
+            mapType = GoogleMap.MAP_TYPE_HYBRID;
+            createMap(mGmap, mapType);
+
+
+        }
+        if(item.getText().equals("None")){
+            Toast.makeText(this, "Changing map type to "+item.getText(),Toast.LENGTH_SHORT).show();
+            mapType = GoogleMap.MAP_TYPE_NONE;
+            createMap(mGmap, mapType);
+
+        }
+
     }
 
 
